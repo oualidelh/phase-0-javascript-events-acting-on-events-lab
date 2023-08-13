61afa1c8850f8a8f3b15484cd10cabb4527ffaea
oualidelhouari <elhouarioualid@gmail.com>
2023-08-13 01:37:43 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+});
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    dodger.style.width = "40px";
+    const dodgerWidth = dodger.style.width.replace("px", "");
+    const width = parseInt(dodgerWidth, 10);
+    console.log(width)
+
+    if (left < 400 - width) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
